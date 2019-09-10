29b370cad65accbd6ab5508e16a99ff756457d93
iskianto20 <iskianto.aris@gmail.com>
2019-09-10 13:22:31 +0700

    first

---
+++ index.php
@@ -0,0 +1,61 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>Home | Aris Iskianto</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" type="text/css" href="style.css">
+</head>
+
+<body>
+
+    <div class="topnav">
+        <a href="#" style="float:right"><img src="img/fbb.png" style="height: 30px; width: 30px; margin-left:10px;"></a>
+        <a href="#" style="float:right"><img src="img/ig.jpg" style="height: 30px; width: 30px; margin-left:10px;"></a>
+        <a href="index.php" style="float:right"><img src="img/linked.png" style="height: 30px; width: 30px; margin-left:10px;"></a>
+    </div>
+
+
+    <div class="leftcolumn">
+        <div class="card">
+            <div class="hero-image">
+                <div class="hero-text">
+                    <h1 style="font-size:50px">I am Aris Iskianto</h1>
+                    <h3>And I'm a Junior Web Programmer</h3>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="content">
+        <img src="img/gue.jpg" alt="aris" width="300" height="300">
+        <h2>Aris Iskianto</h2>
+        <p>Junior Web Programmer</p>
+    </div>
+
+
+    <div class="grid-container">
+        <div class="left" style="background-color: white;">
+            <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+                Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
+                when an unknown printer took a galley of type and scrambled it to make a type specimen book.
+                It has survived not only five centuries, but also the leap into electronic typesetting,
+                remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages,
+                and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+        </div>
+        <div class="right" style="background-color:white;">
+            <p>Age : 23 Years old</p>
+            <p>Phone : 082210813780 </p>
+            <p>Email : Iskianto.aris@gmail.com</p>
+            <p>Hobi : Futsal, Maen Bola.</p>
+            <p>Kota : Bekasi</p>
+        </div>
+    </div>
+
+    <div class="footer">
+        <h2>&copy 2019 Aris Iskianto</h2>
+    </div>
+</body>
+
+</html>
